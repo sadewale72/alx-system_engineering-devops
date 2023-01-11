6cb777c9bc9b4939883df14acd79cbfc04aae421
“sadewale72” <adewaleaderoju202@yahoo.com>
2023-01-11 00:10:56 -0800

    first commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install flask from pip3
-package { 'flask' :
-     ensure   => '2.1.0',
-     provider => 'pip3',
-     }
+# install puppet-flask -v 2.1.0
+package { 'puppet-flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
